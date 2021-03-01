08ea8554a448cef1868e7d773dcab394def08256
Marcos Mont <78879585+404mqs@users.noreply.github.com>
2021-03-01 13:43:44 -0300

    Add files via upload

--- Main.cs
+++ Main.cs
@@ -15,22 +15,6 @@ namespace CommandsRP
         public static MQSPlugin Instance;
 
         public UnityEngine.Color MessageColor { get; private set; }
-        public string AnonFormat { get; private set; }
-        public string AnonIcon { get; private set; }
-        public string EMSFormat { get; private set; }
-        public string EMSIcon { get; private set; }
-        public string MeFormat { get; private set; }
-        public string MeIcon { get; private set; }
-        public string OOCFormat { get; private set; }
-        public string OOCIcon { get; private set; }
-        public string PoliceFormat { get; private set; }
-        public string PoliceIcon { get; private set; }
-        public string SOSFormat { get; private set; }
-        public string SOSIcon { get; private set; }
-        public string StaffFormat { get; private set; }
-        public string StaffIcon { get; private set; }
-        public string TaxiFormat { get; private set; }
-        public string TaxiIcon { get; private set; }
         public string TweetFormat { get; private set; }
         public string TweetIcon { get; private set; }
         protected override void Load()
