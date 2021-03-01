c65c58bff1d47ce5ce70b25944174ec62b06929a
Marcos Mont <78879585+404mqs@users.noreply.github.com>
2021-03-01 13:32:52 -0300

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,56 @@
+﻿using Rocket.API;
+
+
+namespace CommandsRP
+{
+
+    public class Config : IRocketPluginConfiguration
+
+    {
+        public string MessageColor { get; set; }
+        public string AnonFormat;
+        public string AnonIcon;
+        public string EMSFormat;
+        public string EMSIcon;
+        public string MeFormat;
+        public string MeIcon;
+        public string OOCFormat;
+        public string OOCIcon;
+        public string PoliceFormat;
+        public string PoliceIcon;
+        public string SOSFormat;
+        public string SOSIcon;
+        public string StaffFormat;
+        public string StaffIcon;
+        public string TaxiFormat;
+        public string TaxiIcon;
+        public string TweetFormat;
+        public string TweetIcon;
+
+        public void LoadDefaults()
+        {
+            MessageColor = "white";
+            AnonFormat = "{color=#00acee}{b}AnonTweet: {/b}{/color}{color=#E6E6E6}{i} ";
+            AnonIcon = "URL";
+            EMSFormat = "{color=#FF5A5A}[EMS Voice]:{/color}";
+            EMSIcon = "URL";
+            MeFormat = "{color=#E94EFF}[ME] -- !playerName! {/color}: ";
+            MeIcon = "URL";
+            OOCFormat = "{color=#D5D5D5}[OOC] -- !playerName!: {i} ";
+            OOCIcon = "URL";
+            PoliceFormat = "{color=blue}[Police Voice]{/color} ";
+            PoliceIcon = "URL";
+            SOSFormat = "{color=#A9AFAE}[{/color}{color=red}SOS{/color}{color=#A9AFAE}]{/color} " + "!playerName! SEND A SOS SIGNAL. HELP HIM!";
+            SOSIcon = "URL";
+            StaffFormat = "{color=#9224C2}[Server Staff]{/color} ";
+            StaffIcon = "URL";
+            TaxiFormat = "{color=yellow}[Taxi]{/color} -- {color=#00FFFB} !playerName!{/color}: ";
+            TaxiIcon = "URL";
+            TweetFormat = "{color=#00C5FF}[Tweet]{/color}{color=white} @!playerName!:{/color} ";
+            TweetIcon = "URL";
+
+        }
+    }
+}
+
+
---
+++ Main.cs
@@ -0,0 +1,61 @@
+﻿using Rocket.API;
+using Rocket.API.Collections;
+using Rocket.Core.Commands;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+
+namespace CommandsRP
+{
+    public class MQSPlugin : RocketPlugin<Config>
+
+    {
+        public static MQSPlugin Instance;
+
+        public UnityEngine.Color MessageColor { get; private set; }
+        public string AnonFormat { get; private set; }
+        public string AnonIcon { get; private set; }
+        public string EMSFormat { get; private set; }
+        public string EMSIcon { get; private set; }
+        public string MeFormat { get; private set; }
+        public string MeIcon { get; private set; }
+        public string OOCFormat { get; private set; }
+        public string OOCIcon { get; private set; }
+        public string PoliceFormat { get; private set; }
+        public string PoliceIcon { get; private set; }
+        public string SOSFormat { get; private set; }
+        public string SOSIcon { get; private set; }
+        public string StaffFormat { get; private set; }
+        public string StaffIcon { get; private set; }
+        public string TaxiFormat { get; private set; }
+        public string TaxiIcon { get; private set; }
+        public string TweetFormat { get; private set; }
+        public string TweetIcon { get; private set; }
+        protected override void Load()
+        {
+            {
+                MQSPlugin.Instance = this;
+                MessageColor = UnturnedChat.GetColorFromName(Configuration.Instance.MessageColor, UnityEngine.Color.white);
+                Logger.LogWarning("++++++++++++++++++++++++++++++++++++++");
+                Logger.LogWarning($"[{Name}] has been loaded! ");
+                Logger.LogWarning("Dev: MQS#7816");
+                Logger.LogWarning("Join this Discord for Support: https://discord.gg/Ssbpd9cvgp");
+                Logger.LogWarning("++++++++++++++++++++++++++++++++++++++");
+
+            }
+        }
+
+        protected override void Unload()
+        {
+            Logger.LogWarning("++++++++++++++++++++++++++++++++++++++");
+            Logger.LogWarning($"[{Name}] has been unloaded! ");
+            Logger.LogWarning("++++++++++++++++++++++++++++++++++++++");
+
+        }
+
+
+    }
+
+}
